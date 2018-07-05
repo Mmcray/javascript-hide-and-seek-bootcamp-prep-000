752df2d669013aa3c9b7f61dae9a8aea817bdb89
Michael McRay <mmcray5@gmail.com>
2018-07-05 16:42:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  const lis = 
+  document.querySelector('#nested .target');
+  return lis;
+}
+
+function increaseRankBy(n) {
+  const dep = document.querySelectorAll(".ranked-list");
+  
+  for (let i = 0; i < dep.length; i++) {
+    dep[i].innerHTML = (parseInt(i) + n);
+  }
+}
+
+function deepestChild() {
+  const deep = document.getElementById("grand-node");
+  const deeper = deep.querySelectorAll('div');
+  var len = deeper.length;
+  for (let i = 0; i < deeper.length; i++) {
+    while (deeper.length > i) {
+    var deepest = deeper[i];
+    i++;
+    }
+    return deepest;
+  
+  
+  
+  
+}
+}
